b52d76f4d4f08428f474b4c9d2b903ec4fdbac1f
ThePStar7 <39674602+ThePStar7@users.noreply.github.com>
2018-05-28 07:00:04 +0200

    Update and rename CodeCreator.bat to Creator.bat
    Rewrote Creator.bat to make a more efficient Creation Process

---
+++ Creator.bat
@@ -0,0 +1,11 @@
+@echo off
+title Creator
+echo Type any Phrase
+set /p phrase=
+(
+set /p phrase=
+)<new.txt
+certutil -encodehex new.txt enc_new.txt
+certutil -encode enc_new.txt enc64_new.txt
+call enc64_new.txt
+pause
